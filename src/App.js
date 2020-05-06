8c0a2a573b2ce6c3e9f89f08a11e58a148140d30
CKKnot <dalvin.cat@gmail.com>
2020-05-06 22:59:57 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./body.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Layout } from "antd";
+import cubejs from "@cubejs-client/core";
+import { CubeProvider } from "@cubejs-client/react";
+import Header from "./components/Header";
+const API_URL = "http://localhost:4000";
+const CUBEJS_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODg3NzA3OTMsImV4cCI6MTU4ODg1NzE5M30.i_CZKCsCsFxME6CdNdf1UjtY6Jn9hPykpdoIY7hI3Rc";
+const cubejsApi = cubejs(CUBEJS_TOKEN, {
+  apiUrl: `${API_URL}/cubejs-api/v1`
+});
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  }
+}));
 
-function App() {
+const AppLayout = ({ children }) => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Header />
+      <div>{children}</div>
     </div>
   );
-}
+};
+
+const App = ({ children }) => (
+  <CubeProvider cubejsApi={cubejsApi}>
+    <AppLayout>{children}</AppLayout>
+  </CubeProvider>
+);
 
 export default App;
